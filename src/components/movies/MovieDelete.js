b7ce0f7cfd1c909f97a877f6273e0f71451b40e1
FraneCaleta <frane.caleta@yahoo.com>
2022-03-07 20:14:16 +0100

    Reusable modal

--- src/components/movies/MovieDelete.js
+++ src/components/movies/MovieDelete.js
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "../Modal";
 
 const MovieDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
-      StreamDelete
-      <Modal />
+      Delete a movie
+      <Modal
+        title="Delete Movie"
+        content="Are you sure you want to delete this movie?"
+        actions={actions}
+      />
     </div>
   );
 };
